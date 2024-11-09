92cfcb5932daf6cda385736b5413fddd3b9fb10d
dageorge1111 <dageorge1111@gmail.com>
2024-11-09 13:56:35 -0500

    removing files

--- onnx-pb.py
+++
@@ -1,9 +0,0 @@
-from onnx_tf.backend import prepare
-import onnx
-
-# Load the ONNX model
-onnx_model = onnx.load("marianmt_model.onnx")
-tf_rep = prepare(onnx_model)
-print("did stuff starting")
-# Export to TensorFlow SavedModel format
-tf_rep.export_graph("saved_model")
